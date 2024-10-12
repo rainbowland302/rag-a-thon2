b0d0f5395273671904f6339f7ab9ec75e6b03334
Rocky <rockydd@gmail.com>
2024-10-12 15:41:21 -0700

    Show response

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from "@angular/forms";
 import { ClarityModule } from "@clr/angular";
+import { CommonModule } from '@angular/common';
 
 import { AppService } from "./app.service";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, ClarityModule, FormsModule],
+    imports: [RouterOutlet, ClarityModule, FormsModule, CommonModule],
     providers: [AppService],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
@@ -16,15 +17,24 @@ import { AppService } from "./app.service";
 export class AppComponent {
     constructor(
         private service: AppService,
-    ) {
+    ) {}
 
-    }
-
-    message = ""
+    message = "";
+    response: string | null = null;
+    isLoading = false;
 
     search() {
-        this.service.retrieveAns(this.message).subscribe((res: any) => {
-            console.log(res.res);
+        this.isLoading = true;
+        this.response = null;
+        this.service.retrieveAns(this.message).subscribe({
+            next: (res: any) => {
+                this.response = res.res;
+                this.isLoading = false;
+            },
+            error: (err) => {
+                console.error('Error:', err);
+                this.isLoading = false;
+            }
         });
         this.message = "";
     }
