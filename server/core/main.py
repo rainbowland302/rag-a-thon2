f9db6be1f1c9c91be91b3578054245ce71ae598f
Rocky <rockydd@gmail.com>
2024-10-12 16:15:16 -0700

    Move core to server folder

--- server/core/main.py
+++ server/core/main.py
@@ -1,27 +1,27 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 import os
 import json
-import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Get the directory of the current file
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Load the secrets from config.json
-with open('./core/secrets.json', 'r') as file:
+secrets_path = os.path.join(current_dir, 'secrets.json')
+with open(secrets_path, 'r') as file:
     secrets = json.load(file)
 
 os.environ["OPENAI_API_KEY"] = secrets["OPENAI_API_KEY"]
 
 # Load documents and create index (do this once at startup)
-documents = SimpleDirectoryReader("./core/data").load_data()
+data_path = os.path.join(current_dir, 'data')
+documents = SimpleDirectoryReader(data_path).load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
-
 
 def get_response(query):
     response = query_engine.query(query)
     # Convert response to string for easy JSON serialization
     return str(response)
-
 
 if __name__ == '__main__':
     # Example usage
