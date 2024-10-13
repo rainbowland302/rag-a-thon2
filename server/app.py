ddfaadba78abb7d739616cb87f68f7f0797a32d4
Rocky <rockydd@gmail.com>
2024-10-13 08:16:15 -0700

    host static files

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from core.pinecone_steam import get_response
 from dotenv import load_dotenv
 
@@ -9,8 +9,15 @@ load_dotenv()
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/api/search', methods=['POST'])
 def search():
@@ -24,6 +31,5 @@ def search():
         "img": response.get('img')
     })
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)  # Changed host to '0.0.0.0' for VESSL deployment
+    app.run(debug=True, host='0.0.0.0', port=5001)
